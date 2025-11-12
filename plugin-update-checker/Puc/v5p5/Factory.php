00399ea7b6f376cbbe9b454d03225398c444bd80
Dayna Webb <dayna@brokensoundentertainment.com>
2025-11-11 21:00:53 -0500

    Create Factory.php

---
+++ plugin-update-checker/Puc/v5p5/Factory.php
@@ -0,0 +1,8 @@
+<?php
+namespace Puc_v5p5;
+
+class Factory {
+    public static function buildUpdateChecker($metadataUrl, $pluginFile, $slug = '') {
+        return new \stdClass(); // Replace with actual logic if needed
+    }
+}
