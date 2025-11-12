25d615f123a2c14a3f469ee2bb94c4027e6b504d
Dayna Webb <dayna@brokensoundentertainment.com>
2025-11-11 20:03:07 -0500

    Delete admin/admin-page.php

--- admin/admin-page.php
+++
@@ -1,9 +0,0 @@
-<div class="wrap">
-    <h1>Countdown Clock</h1>
-    <p>This is where your countdown clock will be displayed or configured.</p>
-    
-    <div id="countdown-clock">
-        <!-- JavaScript countdown will render here -->
-        <p><strong>Time Remaining:</strong> <span id="clock"></span></p>
-    </div>
-</div>
